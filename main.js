ce9ee59185559bd77e4ad320209caf807cfef76d
Peter Oas <oas.peter@gmail.com>
2018-12-07 16:17:19 -0500

    Working main version

---
+++ main.js
@@ -0,0 +1,76 @@
+let searchButton = document.getElementById('search-button')
+let searchField = document.getElementById('search-field')
+let chooseSong = document.querySelectorAll('search-results')
+
+searchButton.addEventListener('click', function (event) {
+    $.ajax({
+        url: 'https://itunes.apple.com/search', 
+        data: {
+            media: 'music',
+            term: searchField.value
+        },
+        dataType: "json",
+        success: function (coin) {
+            console.log(coin)
+            let searchResults = document.getElementById('search-results')
+            searchResults.innerHTML= ''
+            for (let food of coin.results) {
+                //Creating elements of each song
+                let songItem = document.createElement('div')
+                let songName = document.createElement('div')
+                let songPreview = document.createElement('audio')
+                let songArt = document.createElement('img')
+                let songSource = document.createElement('source')
+                let songInfo = document.createElement('div')
+                let songArtist = document.createElement('div')
+                //CSS template classes
+                songArt.classList.add('trumpet')
+                songItem.classList.add('fl','w-50','w-25-m','w-20-l','pa2')
+                songItem.classList.add('grow','dib','f3-ns','no-underline','black-90')
+                songArt.classList.add('w-100','db','outline','black-10')
+                songInfo.classList.add('mt2','f6','lh-copy')
+                songName.classList.add('ml0','gray','truncate','w-100')
+                songArtist.classList.add('m10','black','truncate','w-100')
+                
+                //song name, artist, and link.
+                songName.innerText = food.trackName
+                songArtist.innerText = food.artistName
+
+                //song music url and info
+                songSource.src = food.previewUrl
+                songSource.type = "audio/mpeg"
+                songPreview.controls = true
+                songPreview.classList.add('w-100')
+                songPreview.appendChild(songSource)
+
+                //song artwork
+                songArt.src = food.artworkUrl100.replace('100x100','550x550')
+                songArt.appendChild(songPreview)
+
+                //putting items into the correct divs
+                songItem.appendChild(songArt)
+                songItem.appendChild(songInfo)
+                songInfo.appendChild(songName)
+                songInfo.appendChild(songArtist)
+                searchResults.appendChild(songItem)
+            }
+            let songPicture = $('.trumpet')
+            console.log(songPicture)
+            $('.trumpet').on('click', play_song)
+        }
+    })
+})
+
+
+function play_song () {
+    chosenSong = event.target
+    console.log(chosenSong)
+    songDisplay = chosenSong.firstChild
+    songDisplay.autoplay = true
+    console.log(songDisplay)
+    let playMusic = document.getElementById('play-music')
+    playMusic.innerHTML = ""
+    // playMusic.appendChild(songDisplay)
+    $(songDisplay).clone().appendTo(playMusic)
+}
+
